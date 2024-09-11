256fb02b6dab16b4b62a2604f48b6ee4b09ec86f
Khadir Abdul <khadir786.ka@googlemail.com>
2024-09-11 01:12:40 +0100

    added bounds to container - partner cannot move out of bounds

--- PokeGotchi/Models/IMoveable.cs
+++ PokeGotchi/Models/IMoveable.cs
@@ -4,7 +4,6 @@ namespace PokeGotchi.Models
 {
     public interface IMoveable
     {
-        public void Walk(int x, int y, Direction direction);
-        public void Run(int x, int y, Direction direction);
+        public void Walk(Direction direction, int numRows, int numColumns);
     }
 }
